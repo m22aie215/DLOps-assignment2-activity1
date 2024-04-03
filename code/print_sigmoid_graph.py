8f5e73df3cc2895c18a982ed8b2bbc751b4a84fa
Prateek Singhal <m22aie215@iitj.ac.in>
2024-04-03 17:19:09 +0530

    print sigmoid

---
+++ code/print_sigmoid_graph.py
@@ -0,0 +1,10 @@
+from activation_functions import Sigmoid, Plotter
+import numpy as np
+
+
+if __name__ == "__main__":
+    # Usage
+    random_values = np.array([-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6])
+    activation_functions = [Sigmoid()]
+    plotter = Plotter(activation_functions, random_values)
+    plotter.plot()
