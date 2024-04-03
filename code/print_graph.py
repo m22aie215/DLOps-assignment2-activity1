d532e1114314fc02802d999ceb1366116b6d75f7
Prateek Singhal <m22aie215@iitj.ac.in>
2024-04-03 17:21:58 +0530

    print relu leakyrelu and tanh graphs

--- code/print_graph.py
+++ code/print_graph.py
@@ -1,10 +1,12 @@
-from activation_functions import Sigmoid, ReLU, LeakyReLU, Tanh, Plotter
+from activation_functions import ReLU, LeakyReLU, Tanh, Plotter
 import numpy as np
 
 
 if __name__ == "__main__":
     # Usage
-    x = np.linspace(-5, 5, 100)
-    activation_functions = [Sigmoid(), ReLU(), LeakyReLU(), Tanh()]
-    plotter = Plotter(activation_functions, x)
+    random_values = np.array(
+        sorted([-3.5, -1.2, 0, 2.8, -4.1, 1.5, -0.7, 3.2, -2.4, 4.6])
+    )
+    activation_functions = [ReLU(), LeakyReLU(), Tanh()]
+    plotter = Plotter(activation_functions, random_values)
     plotter.plot()
